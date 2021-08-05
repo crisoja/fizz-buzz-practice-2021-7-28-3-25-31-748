c71e67a03c96d0baa7aa19c6c39d653f815d76a8
Janley Crsiologo <crisoja@oocl.com>
2021-08-05 14:07:31 +0800

    [FizzBuzzWhizz][Crisologo_Janley] refactor: initialize Buzz as final variable

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,13 +2,14 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
         private static final String FIZZ = "Fizz";
+        private static final String BUZZ = "Buzz";
     public String countOff(int orderNumber) {
 
         if(orderNumber == 3){
             return FIZZ;
         }
         else if(orderNumber == 5){
-            return "Buzz";
+            return BUZZ;
         }
 
         return String.valueOf(orderNumber);
